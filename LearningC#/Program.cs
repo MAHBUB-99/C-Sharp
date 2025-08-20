e04241e7f806ab3666b21e56fca139cd8bd63d6f
Mahbub <mahbub.buet.cse18@gmail.com>
2025-08-20 12:37:12 +0600

    Guess the number

---
+++ LearningC#/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LearningC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(100, 1000);
+
+            Console.WriteLine("You have 10 chance to guess:");
+            int count = 1;
+            while (count<=10)
+            {
+                Console.Write($"{count}th Guess : ");
+                int guess = int.Parse(Console.ReadLine());
+                if (guess == number)
+                {
+                    Console.WriteLine("Congratulation. You guess the correct number.");
+                    return;
+                }
+                else if(guess < number)
+                {
+                    Console.WriteLine("Your guess is too low.");
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is too high.");
+                }
+                count++;
+            }
+            Console.WriteLine("You are so close. Better luck for next time. Number was : "+ number);
+        }
+
+    }
+}
