92545be6c1807eaac809c47638a36b66e891f395
Mahbub <mahbub.buet.cse18@gmail.com>
2025-10-18 11:56:25 +0600

    CRUD operation done

---
+++ EmployeAdminPortal/EmployeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EmployeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeAdminPortal/EmployeAdminPortal/Models/UpdateEmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeAdminPortal.Models
+{
+    public class UpdateEmployeeDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
